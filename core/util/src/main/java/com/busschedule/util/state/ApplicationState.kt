01d02ed677aac37fd9839af421bdd46ac994b24c
DaeYoung Jeong <eodud1025@naver.com>
2025-01-17 15:48:14 +0900

    feat(ApplicationState): applicationState 생성

---
+++ core/util/src/main/java/com/busschedule/util/state/ApplicationState.kt
@@ -0,0 +1,73 @@
+package com.example.connex.ui.domain
+
+import android.net.Uri
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.Stable
+import androidx.navigation.NavHostController
+import com.busschedule.util.constant.Constants
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+@Stable
+class ApplicationState(
+    private val bottomBarState: MutableState<Boolean>,
+    private val navController: NavHostController,
+//    val scaffoldState: ScaffoldState,
+    private val snackbarHostState: SnackbarHostState,
+//    val cameraPositionState: CameraPositionState,
+//    val systmeUiController: SystemUiController,
+    private val coroutineScope: CoroutineScope,
+) {
+    private var currentRoute: String? = Constants.HOME_GRAPH
+    var deepLink: Uri? = null
+
+    fun changeBottomBarVisibility(visibility: Boolean) {
+        bottomBarState.value = visibility
+    }
+
+    fun showSnackbar(message: String) {
+        coroutineScope.launch {
+//            Log.d("daeYoung", "showSnackbar(), snackbarHostState: ${snackbarHostState}")
+            snackbarHostState.showSnackbar(message = message, duration = SnackbarDuration.Short)
+        }
+    }
+
+    fun popBackStack() {
+        navController.popBackStack()
+    }
+
+    fun popBackStackHome() {
+        navController.navigate(Constants.HOME_GRAPH) {
+            popUpTo(navController.graph.id) {
+                inclusive = true
+            }
+        }
+    }
+
+    fun navigate(route: String) {
+        navController.navigate(route)
+        currentRoute = route
+    }
+
+    fun navigate(uri: Uri) {
+        navController.navigate(uri)
+    }
+
+    fun navigateEncodingUrl(prefixUrl: String, encodeUrl: String, param: String) {
+        val encodedUrl = URLEncoder.encode(encodeUrl, StandardCharsets.UTF_8.toString())
+        navController.navigate("$prefixUrl/$encodedUrl/$param")
+    }
+
+    fun navigatePopBackStack(route: String) {
+        navController.navigate(route) {
+            popUpTo(navController.graph.id) {
+                inclusive = true
+            }
+            currentRoute = route
+        }
+    }
+}
