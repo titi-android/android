a7a2fe11ffee8198863de49a50b12089c27ae72b
DaeYoung Jeong <eodud1025@naver.com>
2025-02-04 13:33:44 +0900

    refactor: BusApi URL 수정

--- core/data/src/main/java/com/busschedule/data/network/BusApi.kt
+++ core/data/src/main/java/com/busschedule/data/network/BusApi.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface BusApi {
-    @GET("/api/v1/node/bus-names/all")
+    @GET("/api/v1/nodes/bus-names/all")
     suspend fun readAllBusOfBusStop(
         @Query("cityName") cityName: String,
         @Query("nodeId") busStopId: String,
